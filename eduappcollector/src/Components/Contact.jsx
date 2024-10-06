442e6021e8bde477d36f8e5e0294c542d963b32f
Sandeepa Sineth Wickramasinghe <101142560+Darkhenrhal@users.noreply.github.com>
2024-10-06 13:38:53 +0530

    MyGPA Web completed V - 0.0.1.1

--- eduappcollector/src/Components/Contact.jsx
+++ eduappcollector/src/Components/Contact.jsx
@@ -3,6 +3,10 @@ import { FaGithub } from "react-icons/fa6";
 import { IoIosSend } from "react-icons/io";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'; // Import toastify CSS
+import { BsTelegram } from "react-icons/bs";
+import { IoMail } from "react-icons/io5";
+
+
 
 const Contact = () => {
   
@@ -37,7 +41,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="grid sm:grid-cols-2 items-start gap-16 p-4 mx-auto max-w-6xl">
+    <div className="flex sm:grid-cols-2 items-start gap-16 p-4 mx-auto max-w-6xl">
       {/* Toast Container for notifications */}
       <ToastContainer />
       
@@ -50,16 +54,12 @@ const Contact = () => {
         </p>
 
         <div className="mt-12">
-          <h2 className="text-grey text-base font-bold">Email</h2>
+          <h2 className="text-grey text-base font-bold">Contact</h2>
           <ul className="mt-4">
             <li className="flex items-center">
               <div className="bg-white h-10 w-10 rounded-full flex items-center justify-center hover:shadow-lg hover:shadow-purple hover:scale-105 transition-all duration-300 ease-in-out shrink-0">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#8471BE'
-                  viewBox="0 0 479.058 479.058" className="transition-transform duration-300 ease-in-out">
-                  <path
-                    d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-                    data-original="#000000" />
-                </svg>
+              <IoMail className="text-3xl text-purple" />
+
               </div>
 
               <a href="javascript:void(0)" className="text-purple text-sm ml-4">
@@ -67,16 +67,27 @@ const Contact = () => {
                 <strong>sineth21404@gmail.com</strong>
               </a>
             </li>
+            <li className="flex items-center mt-5">
+              <div className="bg-white h-10 w-10 rounded-full flex items-center justify-center hover:shadow-lg hover:shadow-purple hover:scale-105 transition-all duration-300 ease-in-out shrink-0">
+                <BsTelegram className="text-3xl text-purple" />
+
+              </div>
+
+              <a href="https://t.me/+YazfAzt1dihkMjJl" target="_blank" className="text-purple text-sm ml-4">
+                <small className="block">Telegram</small>
+                <strong>Link</strong>
+              </a>
+            </li>
           </ul>
         </div>
 
-        <div className="mt-12">
+        <div className="mt-8">
           <h2 className="text-gray-800 text-base font-bold">Check our Github Repository...</h2>
 
           <ul className="flex mt-4 space-x-4">
             <li className="bg-white h-10 w-10 rounded-full flex items-center justify-center hover:shadow-purple hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out shrink-0">
               <a href="https://github.com/Darkhenrhal/MyGPA.git" target="_blank">
-                <FaGithub className="text-2xl text-purple transition-transform duration-300 ease-in-out" />
+                <FaGithub className="text-3xl text-purple transition-transform duration-300 ease-in-out" />
               </a>
             </li>
           </ul>
--- eduappcollector/src/Components/Footer.jsx
+++ eduappcollector/src/Components/Footer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { IoMail } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa6";
+
+
 
 
 const Footer = () => {
+      const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        
+        formData.append("access_key", "a75430a1-81fb-43c8-add5-20bcb10f7aa3");
+        
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+        
+        try {
+          const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: json
+          }).then((res) => res.json());
+        
+          if (res.success) {
+            toast.success("Message sent successfully!");
+            event.target.reset(); // Clear the form fields
+          } else {
+            toast.error("Failed to send message. Please try again.");
+          }
+        } catch (error) {
+          toast.error("An error occurred. Please try again later.");
+        }
+      };
+
     return (
         <footer class="font-sans tracking-wide bg-grey py-12 px-16">
         <div class="grid max-md-grid-cols-1 lg:grid-cols-2 gap-20">
@@ -16,11 +50,8 @@ const Footer = () => {
             <ul class="grid mt-6 sm:grid-cols-2  gap-2">
               <li class="flex items-center max-sm:mb-8">
                 <div class="bg-[#343538] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#fff' viewBox="0 0 482.6 482.6">
-                    <path
-                      d="M98.339 320.8c47.6 56.9 104.9 101.7 170.3 133.4 24.9 11.8 58.2 25.8 95.3 28.2 2.3.1 4.5.2 6.8.2 24.9 0 44.9-8.6 61.2-26.3.1-.1.3-.3.4-.5 5.8-7 12.4-13.3 19.3-20 4.7-4.5 9.5-9.2 14.1-14 21.3-22.2 21.3-50.4-.2-71.9l-60.1-60.1c-10.2-10.6-22.4-16.2-35.2-16.2-12.8 0-25.1 5.6-35.6 16.1l-35.8 35.8c-3.3-1.9-6.7-3.6-9.9-5.2-4-2-7.7-3.9-11-6-32.6-20.7-62.2-47.7-90.5-82.4-14.3-18.1-23.9-33.3-30.6-48.8 9.4-8.5 18.2-17.4 26.7-26.1 3-3.1 6.1-6.2 9.2-9.3 10.8-10.8 16.6-23.3 16.6-36s-5.7-25.2-16.6-36l-29.8-29.8c-3.5-3.5-6.8-6.9-10.2-10.4-6.6-6.8-13.5-13.8-20.3-20.1-10.3-10.1-22.4-15.4-35.2-15.4-12.7 0-24.9 5.3-35.6 15.5l-37.4 37.4c-13.6 13.6-21.3 30.1-22.9 49.2-1.9 23.9 2.5 49.3 13.9 80 17.5 47.5 43.9 91.6 83.1 138.7zm-72.6-216.6c1.2-13.3 6.3-24.4 15.9-34l37.2-37.2c5.8-5.6 12.2-8.5 18.4-8.5 6.1 0 12.3 2.9 18 8.7 6.7 6.2 13 12.7 19.8 19.6 3.4 3.5 6.9 7 10.4 10.6l29.8 29.8c6.2 6.2 9.4 12.5 9.4 18.7s-3.2 12.5-9.4 18.7c-3.1 3.1-6.2 6.3-9.3 9.4-9.3 9.4-18 18.3-27.6 26.8l-.5.5c-8.3 8.3-7 16.2-5 22.2.1.3.2.5.3.8 7.7 18.5 18.4 36.1 35.1 57.1 30 37 61.6 65.7 96.4 87.8 4.3 2.8 8.9 5 13.2 7.2 4 2 7.7 3.9 11 6 .4.2.7.4 1.1.6 3.3 1.7 6.5 2.5 9.7 2.5 8 0 13.2-5.1 14.9-6.8l37.4-37.4c5.8-5.8 12.1-8.9 18.3-8.9 7.6 0 13.8 4.7 17.7 8.9l60.3 60.2c12 12 11.9 25-.3 37.7-4.2 4.5-8.6 8.8-13.3 13.3-7 6.8-14.3 13.8-20.9 21.7-11.5 12.4-25.2 18.2-42.9 18.2-1.7 0-3.5-.1-5.2-.2-32.8-2.1-63.3-14.9-86.2-25.8-62.2-30.1-116.8-72.8-162.1-127-37.3-44.9-62.4-86.7-79-131.5-10.3-27.5-14.2-49.6-12.6-69.7z"
-                      data-original="#000000" />
-                  </svg>
+                  <FaPhone className="text-1xl text-greylight"  />
+
                 </div>
                 <a href="javascript:void(0)" class="text-gray-100 text-sm ml-4">
                   <small class="block">Tel</small>
@@ -29,12 +60,8 @@ const Footer = () => {
               </li>
               <li class="flex items-center">
                 <div class="bg-[#343538] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#fff'
-                    viewBox="0 0 479.058 479.058">
-                    <path
-                      d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-                      data-original="#000000" />
-                  </svg>
+                  <IoMail className="text-2xl text-greylight" />
+
                 </div>
                 <a href="javascript:void(0)" class="text-gray-100 text-sm ml-4">
                   <small class="block">Mail</small>
@@ -48,12 +75,24 @@ const Footer = () => {
             <h4 class="text-gray-100 font-bold text-lg">For Collaboration</h4>
             <p class="text-sm mt-6 text-gray-300 text-justify">We’re on a mission to build the largest free and open-source app store for educational tools. Whether you're a developer, educator, or simply passionate about open-source projects, we welcome your collaboration.</p>
   
-            <div class="bg-[#343538] flex px-2 py-1 rounded-full mt-12">
-              <input type='email' placeholder='Enter your email'
-                class="w-full outline-none text-gray-300 text-sm bg-transparent pl-4" />
-              <button type='button'
-                class="bg-gray-100 hover:bg-white transition-all text-gray-600 font-semibold text-sm rounded-full px-5 py-2 ml-4">Submit</button>
-            </div>
+            <form onSubmit={onSubmit} className="bg-[#343538] flex px-2 py-1 rounded-full mt-12">
+              <input
+                type='email'
+                name="email"
+                placeholder='Enter your email'
+                className="w-full outline-none text-gray-300 text-sm bg-[#343538] pl-4 placeholder:text-gray-500 transition-all duration-200"
+                // Remove the onFocus and onBlur handlers
+              />
+              <button
+                type='submit'
+                className="bg-gray-100 hover:bg-white transition-all text-gray-600 font-semibold text-sm rounded-full px-5 py-2 ml-4"
+              >
+                Submit
+              </button>
+            </form>
+
+
+
   
             {/* <ul class="flex items-center justify-end mt-8 space-x-4">
               <li class="bg-[#343538] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
